06f9b0e1ad6799b82ea3e0deddb4a301c230cb91
CodeProject <Webmaster@codeproject.com>
2014-02-11 13:43:51 -0500

    New version commited via file archive upload at 11-Feb-14  13:43

---
+++ Article_src/Restaurant/RestaurantOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    public class RestaurantOrder : Collection<OrderItem>
+    {
+        public RestaurantOrder()
+        {
+        }
+
+        public RestaurantOrder(IList<OrderItem> list)
+            : base(list)
+        {
+        }
+    }
+}
