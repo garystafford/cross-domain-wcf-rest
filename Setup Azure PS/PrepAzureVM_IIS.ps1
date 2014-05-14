b93581f2b7cf4680ad404f7c5919dba67f13304f
Gary A. Stafford <garystafford@rochester.rr.com>
2014-05-14 19:33:02 -0400

    Fixing PS script for IIS Application Pool on Azure VM.

--- Setup Azure PS/PrepAzureVM_IIS.ps1
+++ Setup Azure PS/PrepAzureVM_IIS.ps1
@@ -9,12 +9,12 @@
 $newSite = "MenuWcfRestService"
 
 if (-not (Test-Path IIS:\Sites\$newSite)){
-    New-Website -Name $newSite -Port 9250 -PhysicalPath c:\$newSite -ApplicationPool "ASP.NET v4.0"
+    New-Website -Name $newSite -Port 9250 -PhysicalPath c:\$newSite -ApplicationPool "DefaultAppPool"
 }
 
 # Create WCF service website
 $newSite = "RestaurantDemoSite"
 
 if (-not (Test-Path IIS:\Sites\$newSite)){
-    New-Website -Name $newSite -Port 9255 -PhysicalPath c:\$newSite -ApplicationPool "ASP.NET v4.0"
+    New-Website -Name $newSite -Port 9255 -PhysicalPath c:\$newSite -ApplicationPool "DefaultAppPool"
 }
