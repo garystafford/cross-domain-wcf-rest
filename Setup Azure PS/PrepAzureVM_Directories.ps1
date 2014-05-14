67a4f53c9f13bed89f00abe2d03cc7201358c611
Gary Stafford <gstafford@paychex.com>
2014-05-14 19:19:34 -0400

    Adding PowerShell setup scripts for Azure.

---
+++ Setup Azure PS/PrepAzureVM_Directories.ps1
@@ -0,0 +1,63 @@
+﻿###############################################################
+# Author: Gary A. Stafford
+# Revised: 2014-05-14
+# Prep Azure VM for 'cross-domain-wcf-rest' project deployment
+# Create directories and security settings for (3) directories
+###############################################################
+
+#The possible values for Rights are 
+# ListDirectory, ReadData, WriteData 
+# CreateFiles, CreateDirectories, AppendData 
+# ReadExtendedAttributes, WriteExtendedAttributes, Traverse
+# ExecuteFile, DeleteSubdirectoriesAndFiles, ReadAttributes 
+# WriteAttributes, Write, Delete 
+# ReadPermissions, Read, ReadAndExecute 
+# Modify, ChangePermissions, TakeOwnership
+# Synchronize, FullControl
+
+
+# Orders directory
+$newDirectory = "c:\RestaurantOrders"
+
+if (-not (Test-Path $newDirectory)){
+    New-Item -Type directory -Path $newDirectory
+}
+
+$acl = Get-Acl $newDirectory
+$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(
+    "INTERACTIVE","Modify","ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.SetAccessRule($ar)
+Set-Acl $newDirectory $acl
+
+
+# Website directory
+$newDirectory = "c:\RestaurantDemoSite"
+
+if (-not (Test-Path $newDirectory)){
+    New-Item -Type directory -Path $newDirectory
+}
+
+$acl = Get-Acl $newDirectory
+$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(
+    "IUSR","ReadAndExecute","ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.SetAccessRule($ar)
+Set-Acl $newDirectory $acl
+
+
+# WCF service directory
+$newDirectory = "c:\MenuWcfRestService"
+
+if (-not (Test-Path $newDirectory)){
+    New-Item -Type directory -Path $newDirectory
+}
+
+$acl = Get-Acl $newDirectory
+$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(
+    "IUSR","ReadAndExecute","ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.SetAccessRule($ar)
+
+Set-Acl $newDirectory $acl
+$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(
+    "IIS_IUSRS","ReadAndExecute","ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.SetAccessRule($ar)
+Set-Acl $newDirectory $acl
---
+++ Setup Azure PS/PrepAzureVM_IIS.ps1
@@ -0,0 +1,20 @@
+﻿###############################################################
+# Author: Gary A. Stafford
+# Revised: 2014-05-14
+# Prep Azure VM for 'cross-domain-wcf-rest' project deployment
+# Create main website and WCF service website
+###############################################################
+
+# Create main website
+$newSite = "MenuWcfRestService"
+
+if (-not (Test-Path IIS:\Sites\$newSite)){
+    New-Website -Name $newSite -Port 9250 -PhysicalPath c:\$newSite -ApplicationPool "ASP.NET v4.0"
+}
+
+# Create WCF service website
+$newSite = "RestaurantDemoSite"
+
+if (-not (Test-Path IIS:\Sites\$newSite)){
+    New-Website -Name $newSite -Port 9255 -PhysicalPath c:\$newSite -ApplicationPool "ASP.NET v4.0"
+}
