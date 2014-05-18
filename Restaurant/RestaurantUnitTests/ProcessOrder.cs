9628e1855fd7c3ed01c9b23b0b8f8f5c5b2d5ddb
Gary A. Stafford <garystafford@rochester.rr.com>
2014-05-18 01:16:15 -0400

    Adding initial unit tests to project.

---
+++ Restaurant/RestaurantUnitTests/ProcessOrder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurant;
+using System.IO;
+
+namespace RestaurantUnitTests
+{
+    [TestClass]
+    public class ProcessOrder
+    {
+        private Restaurant.ProcessOrder processOrder = new Restaurant.ProcessOrder();
+
+        [TestMethod]
+        public void Test_JsonFilePath_Exists()
+        {
+            bool jsonFilePathExists = Directory.Exists(Restaurant.ProcessOrder.STR_JsonFilePath);
+            Assert.IsTrue(jsonFilePathExists);
+        }
+
+        [TestMethod]
+        public void Test_ProcessOrderJSON_ReturnValue_IsNotNull()
+        {
+            String restaurantOrder = "{'restaurantOrder':[{'Quantity':'1','Id':'4'}]}";
+            Assert.IsNotNull(processOrder.ProcessOrderJSON(restaurantOrder));
+        }
+
+        [TestMethod]
+        public void Test_ProcessOrderJSON_ItemCount_IsOne()
+        {
+            String restaurantOrder = "{'restaurantOrder':[{'Quantity':'1','Id':'4'}]}";
+            OrderResponse orderResponse = processOrder.ProcessOrderJSON(restaurantOrder);
+            Assert.AreEqual(orderResponse.ItemCount, 1);
+        }
+
+        [TestMethod]
+        public void Test_ProcessOrderJSON_OrderMessage_NotError()
+        {
+            String restaurantOrder = "{'restaurantOrder':[{'Quantity':'1','Id':'4'}]}";
+            OrderResponse orderResponse = processOrder.ProcessOrderJSON(restaurantOrder);
+            Assert.AreEqual(orderResponse.OrderMessage, "Thank you for your order!");
+        }
+    }
+}
