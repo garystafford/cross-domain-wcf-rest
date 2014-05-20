82d77336ec8a0f4988dd7b03b47b092959e53087
Gary Stafford <gstafford@paychex.com>
2014-05-20 12:51:35 -0400

    minor changes to profiles.

--- Setup Azure PS/Deploy_Projects_AppVeyor.ps1
+++ Setup Azure PS/Deploy_Projects_AppVeyor.ps1
@@ -4,12 +4,10 @@
 # AppVeyor Script to Deploy Project to Azure VM
 ###############################################################
 
-# WCF Service
-cd "C:\projects\cross-domain-wcf-rest\Restaurant\RestaurantWcfService"
-msbuild RestaurantWcfService.csproj /p:DeployOnBuild=true /p:PublishProfile=AzureVM /p:VisualStudioVersion=12.0 `
-	/p:Configuration=Release /t:WebPublish /p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
+# Build and Deploy WCF Service
+msbuild Restaurant\RestaurantWcfService\RestaurantWcfService.csproj /p:DeployOnBuild=true /p:PublishProfile=AzureVM `
+/p:VisualStudioVersion=12.0 /p:Configuration=AppVeyor /p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
 
-# Website
-cd "C:\projects\cross-domain-wcf-rest\Restaurant\RestaurantDemoSite"
-msbuild website.publishproj /p:DeployOnBuild=true /p:PublishProfile=AzureVM /p:VisualStudioVersion=12.0 `
-	/p:Configuration=Release /t:WebPublish /p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
+# Build and Deploy Website
+msbuild Restaurant\RestaurantDemoSite\website.publishproj /p:DeployOnBuild=true /p:PublishProfile=AzureVM `
+/p:VisualStudioVersion=12.0 /p:Configuration=Release /p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
