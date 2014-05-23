e506d37ab01611b2bc243d356b9089bd5b9f3e9f
Gary Stafford <gstafford@paychex.com>
2014-05-23 14:41:21 -0400

    minor changes to profiles and scripts.

--- PowerShell Setup Scripts/Deploy_Projects_AppVeyor.ps1
+++ PowerShell Setup Scripts/Deploy_Projects_AppVeyor.ps1
@@ -1,18 +1,16 @@
 ###############################################################
 # Author: Gary A. Stafford
-# Revised: 2014-05-20
+# Revised: 2014-05-23
 # AppVeyor Script to Deploy Project to Azure VM
 ###############################################################
 
-# Build and Deploy WCF Service
-# (AppVeyor Configuration ignores website Project in Solution)
+# Build and deploy WCF service
+# (AppVeyor config ignores website Project in Solution)
 msbuild Restaurant\Restaurant.sln `
-/p:DeployOnBuild=true /p:PublishProfile=AzureVM `
-/p:Configuration=AppVeyor /p:AllowUntrustedCertificate=true `
-/p:Password=$env:AZURE_VM_PASSWORD
+/p:DeployOnBuild=true /p:PublishProfile=AzureVM /p:Configuration=AppVeyor `
+/p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
 
-# Build and Deploy RestaurantDemoSite Project (website)
+# Build and deploy website
 msbuild Restaurant\RestaurantDemoSite\website.publishproj `
-/p:DeployOnBuild=true /p:PublishProfile=AzureVM `
-/p:Configuration=Release /p:AllowUntrustedCertificate=true `
-/p:Password=$env:AZURE_VM_PASSWORD
+/p:DeployOnBuild=true /p:PublishProfile=AzureVM /p:Configuration=Release `
+/p:AllowUntrustedCertificate=true /p:Password=$env:AZURE_VM_PASSWORD
